65300ec2bdd403ca9d919d8cfd3066f86bccf516
mmazu <mazurak.marian.ukr@gmail.com>
2020-12-07 17:59:40 +0200

    First commit

---
+++ src/main/java/com/mmazu/dockerjavatravis/DockerJavaTravisApplication.java
@@ -0,0 +1,13 @@
+package com.mmazu.dockerjavatravis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJavaTravisApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DockerJavaTravisApplication.class, args);
+    }
+}
